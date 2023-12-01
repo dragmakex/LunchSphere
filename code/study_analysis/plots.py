08adcad9b1b84ca25f7feb783d040b82f2831719
Nishanth Kumar <nishanthkumar2002@gmail.com>
2023-12-01 17:51:06 +0100

    test

--- code/study_analysis/plots.py
+++ code/study_analysis/plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 from scipy.stats import shapiro
 from scipy.stats import levene
@@ -11,11 +12,15 @@ file_path = path_to_analysis + '/user_study_data.csv'
 df = pd.read_csv(file_path)
 
 
-# plotting SUS values of A and B as histogram
+
+##### 1. Dependent variable: SUS values #####
+
 number_of_bins = 20
 
 # A
-plt.hist(df['sus_a'], bins=number_of_bins, color='blue', edgecolor='black', alpha=0.7)
+plt.hist(df['sus_a'], range=(40,100), bins=number_of_bins, color='blue', edgecolor='black', alpha=0.7)
+plt.xticks(np.arange(40,101,5))
+plt.yticks(np.arange(0, 4, 1)) 
 plt.xlabel('SUS Value')
 plt.ylabel('Frequency')
 plt.title(f'Histogram of SUS Values of A (Bin Size: {number_of_bins})')
@@ -24,7 +29,9 @@ plt.savefig(path_to_analysis + '/histogram_sus_values_A')
 plt.close()
 
 # B
-plt.hist(df['sus_b'], bins=number_of_bins, color='red', edgecolor='black', alpha=0.7)
+plt.hist(df['sus_b'], range=(40,100), bins=number_of_bins, color='red', edgecolor='black', alpha=0.7)
+plt.xticks(np.arange(40,101,5))
+plt.yticks(np.arange(0, 4, 1)) 
 plt.xlabel('SUS Value')
 plt.ylabel('Frequency')
 plt.title(f'Histogram of SUS Values of B (Bin Size: {number_of_bins})')
@@ -42,17 +49,87 @@ shapiro_res_b = shapiro(df['sus_b'])
 # ...
 
 
+
+##### 2. Dependent variable: Time needed to select a group
+
+number_of_bins = 24
+
+# A
+plt.hist(df['time_a'], range=(0,120), bins=number_of_bins, color='blue', edgecolor='black', alpha=0.7)
+plt.xticks(np.arange(0,121,5))
+plt.yticks(np.arange(0, 4, 1)) 
+plt.xlabel('Time Needed to Select a Group')
+plt.ylabel('Frequency')
+plt.title(f'Histogram of Time Needed to Select a Group in A Test (Bin Size: {number_of_bins})')
+
+plt.savefig(path_to_analysis + '/histogram_time_A')
+plt.close()
+
+# B
+plt.hist(df['time_b'], range=(0,120), bins=number_of_bins, color='blue', edgecolor='black', alpha=0.7)
+plt.xticks(np.arange(0,121,5))
+plt.yticks(np.arange(0, 4, 1)) 
+plt.xlabel('Time Needed to Select a Group')
+plt.ylabel('Frequency')
+plt.title(f'Histogram of Time Needed to Select a Group in B Test (Bin Size: {number_of_bins})')
+
+plt.savefig(path_to_analysis + '/histogram_time_B')
+plt.close()
+
+
+# Do Shapiro-Wilke Test
+shapiro_res_a = shapiro(df['time_a'])
+shapiro_res_b = shapiro(df['time_b'])
+
+
+# Do Levene Test
+# ...
+
+
+
+
+
 # Write Results to Markdown File
 with open(path_to_analysis + '/results.md', 'w') as file:
     file.write('# Data Analysis Results\n')
     file.write('## Properties of Data\n')
 
+
     file.write('### Distribution of SUS Values in A Test\n')
-    html_img_txt = '<img src="histogram_sus_values_A.png" alt="Distribution of SUS Values in A Test" width="500"/>'
-    file.write(html_img_txt + '\n')
+    html_img_txt = '<img src="histogram_sus_values_A.png" alt="Distribution of SUS Values in A Test" width="400"/>'
+    file.write(html_img_txt + '\n\n')
+
+    file.write('### Result of Shapiro-Wilke Test A\n')
+    file.write('p-value: ' + str(round(shapiro_res_a[1],3)) + '\n')
+    if (shapiro_res_a[1] > 0.05): file.write('-> We can assume a normal distribution.\n') 
+    else: file.write('-> We cannot assume a normal distribution.')
 
     file.write('### Distribution of SUS Values in B Test\n')
-    html_img_txt = '<img src="histogram_sus_values_B.png" alt="Distribution of SUS Values in B Test" width="500"/>'
-    file.write(html_img_txt + '\n')
+    html_img_txt = '<img src="histogram_sus_values_B.png" alt="Distribution of SUS Values in B Test" width="400"/>'
+    file.write(html_img_txt + '\n\n')
+
+    file.write('### Result of Shapiro-Wilke Test B\n')
+    file.write('p-value: ' + str(round(shapiro_res_b[1],3)) + '\n')
+    if (shapiro_res_b[1] > 0.05): file.write('-> We can assume a normal distribution.\n') 
+    else: file.write('-> We cannot assume a normal distribution.')
+
+
+    file.write('### Distribution of Time Needed to Select a Group in A Test\n')
+    html_img_txt = '<img src="histogram_time_A.png" alt="Distribution of Time Needed to Select a Group in A Test" width="400"/>'
+    file.write(html_img_txt + '\n\n')
+
+    file.write('### Result of Shapiro-Wilke Test A\n')
+    file.write('p-value: ' + str(round(shapiro_res_a[1],3)) + '\n')
+    if (shapiro_res_a[1] > 0.05): file.write('-> We can assume a normal distribution.\n') 
+    else: file.write('-> We cannot assume a normal distribution.')
+
+    file.write('### Distribution of Time Needed to Select a Group in B Test\n')
+    html_img_txt = '<img src="histogram_time_B.png" alt="Distribution of Time Needed to Select a Group in B Test" width="400"/>'
+    file.write(html_img_txt + '\n\n')
+
+    file.write('### Result of Shapiro-Wilke Test B\n')
+    file.write('p-value: ' + str(round(shapiro_res_b[1],3)) + '\n')
+    if (shapiro_res_b[1] > 0.05): file.write('-> We can assume a normal distribution.\n') 
+    else: file.write('-> We cannot assume a normal distribution.')
     
 
--- code/study_analysis/results.md
+++ code/study_analysis/results.md
@@ -1,6 +1,26 @@
 # Data Analysis Results
 ## Properties of Data
 ### Distribution of SUS Values in A Test
-<img src="histogram_sus_values_A.png" alt="Distribution of SUS Values in A Test" width="500"/>
+<img src="histogram_sus_values_A.png" alt="Distribution of SUS Values in A Test" width="400"/>
+
+### Result of Shapiro-Wilke Test A
+p-value: 0.091
+-> We can assume a normal distribution.
 ### Distribution of SUS Values in B Test
-<img src="histogram_sus_values_B.png" alt="Distribution of SUS Values in B Test" width="500"/>
+<img src="histogram_sus_values_B.png" alt="Distribution of SUS Values in B Test" width="400"/>
+
+### Result of Shapiro-Wilke Test B
+p-value: 0.329
+-> We can assume a normal distribution.
+### Distribution of Time Needed to Select a Group in A Test
+<img src="histogram_time_A.png" alt="Distribution of Time Needed to Select a Group in A Test" width="400"/>
+
+### Result of Shapiro-Wilke Test A
+p-value: 0.091
+-> We can assume a normal distribution.
+### Distribution of Time Needed to Select a Group in B Test
+<img src="histogram_time_B.png" alt="Distribution of Time Needed to Select a Group in B Test" width="400"/>
+
+### Result of Shapiro-Wilke Test B
+p-value: 0.329
+-> We can assume a normal distribution.
