69195ab7378af1b3fc0f707b135296493a2d78e6
Andri Bernhardsgruetter <andribgruetter@gmail.com>
2023-12-04 13:19:26 +0100

    added more functionality

--- code/study_analysis/plots.py
+++ code/study_analysis/plots.py
@@ -4,12 +4,23 @@ import numpy as np
 
 from scipy.stats import shapiro
 from scipy.stats import levene
+from scipy.stats import ttest_rel
 
-path_to_analysis = 'C:/Users/nisha/OneDrive/Desktop/ETH/human_computer_interaction/hci-project-hci2023-group-16/code/study_analysis'
+#path_to_analysis = 'C:/Users/nisha/OneDrive/Desktop/ETH/human_computer_interaction/hci-project-hci2023-group-16/code/study_analysis'
+path_to_analysis = '/Users/andri/Documents/Local Stuff/Programming/Python/HCI/hci-project-hci2023-group-16/code/study_analysis'
 
 # make dataframe from csv file containing all study results 
 file_path = path_to_analysis + '/user_study_data.csv'
 df = pd.read_csv(file_path)
+
+# user demographics
+mean_age = df['age'].mean()
+print("Mean Age:", mean_age)
+
+male_count = df['gender'].value_counts().get('male', 0)
+total_count = len(df['gender'])
+male_percentage = (male_count / total_count) * 100
+
 
 
 
@@ -46,8 +57,30 @@ shapiro_res_b = shapiro(df['sus_b'])
 
 
 # Do Levene Test
-# ...
+# Separate the data into the groups
+group_a = df[df['a_or_b'] == 'a']
+group_b = df[df['a_or_b'] == 'b']
 
+# Perform Levene's test for 'sus_a'
+levene_test_sus_a = levene(group_a['sus_a'], group_b['sus_a'])
+
+# Perform Levene's test for 'sus_b'
+levene_test_sus_b = levene(group_a['sus_b'], group_b['sus_b'])
+
+#perform levene test differently:
+levene_test_both = levene(df['sus_a'],df['sus_b'])
+
+# Output the results
+print("Levene's test for 'sus_a':", levene_test_sus_a)
+print("Levene's test for 'sus_b':", levene_test_sus_b)
+print("Levene's test for 'both a and b':", levene_test_both)
+
+
+
+# Perform the paired t-test
+ttest_result = ttest_rel(df['sus_a'], df['sus_b'])
+
+print("Paired t-test result:", ttest_result)
 
 
 ##### 2. Dependent variable: Time needed to select a group
@@ -94,6 +127,9 @@ with open(path_to_analysis + '/results.md', 'w') as file:
     file.write('# Data Analysis Results\n')
     file.write('## Properties of Data\n')
 
+    file.write('### Participant demographics\n')
+    file.write('Mean age:'+ str(mean_age) +'\n')
+    file.write('Gender distribution (in %)' +str(male_percentage) + '\n')
 
     file.write('### Distribution of SUS Values in A Test\n')
     html_img_txt = '<img src="histogram_sus_values_A.png" alt="Distribution of SUS Values in A Test" width="400"/>'
